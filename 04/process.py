64599f1a878011baea9445b0604acf685cd899f4
cptnalf <captainalf@gmail.com>
2017-12-05 12:22:14 -0500

    complete the 4th day part 2

--- 04/process.py
+++ 04/process.py
@@ -8,13 +8,16 @@ def process(infile):
   """
 
   valids = 0
+  valid2s = 0
   cnt = 0
   inf = open(infile, 'r')
   for l in inf:
     cnt += 1
     d1 = []
     isok = True
-    for w in l.split():
+    hasana = False
+    llst = l.split()
+    for w in llst:
       if w in d1:
         isok = False
         break
@@ -23,8 +26,44 @@ def process(infile):
     
     if isok:
       valids += 1
+
+      z = 0
+      for w in range(len(llst)):
+        z = w + 1
+        while z < len(llst):
+          hasana = checkanagram(llst[w], llst[z])
+          if hasana:
+            break
+          
+          z +=1
+        if hasana:
+          break
+      
+      if not hasana:
+        print (str(hasana) + ' ' + str(llst))
+        valid2s += 1
   
   print("have {0} valids of {1}".format(valids, cnt))
+  print("2: have {0} valids of {1}".format(valid2s, cnt))
+
+def checkanagram(w1, w2):
+  
+  isana = True
+  l1 = []
+  for c in w1:
+    l1.append(c)
+
+  for c in w2:
+    if c in l1:
+      l1.remove(c)
+    else:
+      isana = False
+      break
+    
+  if isana:
+    isana = len(l1) == 0
+
+  return isana
 
 if __name__ == "__main__":
   process(sys.argv[1])
