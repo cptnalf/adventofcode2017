77eb8193d1b6a3fa099d1d79e8dc88609564a095
cptnalf <captainalf@gmail.com>
2017-12-05 11:18:37 -0500

    part 2 of day 2.

--- 02/process.py
+++ 02/process.py
@@ -8,27 +8,49 @@ def process(args):
   inf = open(args[0], 'r')
 
   lines = []
+  lines1 = []
 
   for l in inf:
     lw = None
     up = None
     v = 0
+    lst1 = []
     for c in l.split('\t'):
       v = int(c)
+      lst1.append(v)
       if lw == None or lw > v:
         lw = v
       if up == None or up < v:
         up = v
     lines.append([lw, up])
-  
+
+    lst1.sort()
+    print(lst1)
+    for x in range(len(lst1)):
+      y = len(lst1) -1
+      while y >= 0:
+        if y == x:
+          y -= 1
+          continue
+        else:
+          if lst1[y] % lst1[x] == 0:
+            lines1.append(lst1[y] // lst1[x])
+            break
+        y -= 1
+
+    
   inf.close()
   inf = None
   print(lines)
+  print(lines1)
 
   tot = 0
   for sl in lines:
     tot += sl[1] - sl[0]
-
+  print(tot)
+  tot = 0
+  for ls in lines1:
+    tot += ls
   print(tot)
 
 if __name__ == '__main__':
