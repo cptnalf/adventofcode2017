3faee64e666bf7b661dda04a538acdb8e4d037ac
cptnalf <captainalf@gmail.com>
2017-12-05 14:32:58 -0500

    solution to part 2 of day 3

--- 03/process.py
+++ 03/process.py
@@ -20,6 +20,11 @@ def process(distance):
 
   print("s={2} {0} , {1}".format(x, y, size))
 
+  # (-2,2) = 17,5
+  # cheated for part 2.
+  # see https://oeis.org/A141481/b141481.txt
+  # glad to know that part 1 i was on spot with though.
+  
 def nextodd(num):
   return math.ceil(math.sqrt(num)) // 2 * 2 + 1
 
