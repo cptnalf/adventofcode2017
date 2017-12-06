6cc1184decace1d19ed55240a399953b646fad87
cptnalf <captainalf@gmail.com>
2017-12-06 10:45:42 -0500

    day 6 part 1

---
+++ 06/process.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+import sys
+
+def process(infile):
+  prevs = []
+  cycle = 0
+  lst = slurp(infile)
+  while True:
+    prevs.append(lst)
+    print(lst)
+    
+    newlst = level_lst(lst)
+    cycle += 1
+    if newlst in prevs:
+      break
+    lst = newlst
+  
+  print("{0:d} cycles for :{1}".format(cycle, newlst))
+
+def slurp(fname):
+  lst = []
+  inf = open(fname,'r')
+  for l in inf:
+    for w in l.split():
+      i = int(w)
+      lst.append(i)
+  
+  return lst
+
+def level_lst(inlst):
+  outlst = []
+
+  # start point
+  idxm = 0
+  for x in range(len(inlst)):
+    if inlst[idxm] < inlst[x]:
+      idxm = x
+  
+  amt = inlst[idxm]
+  outlst = inlst.copy()
+  outlst[idxm] = 0
+  idxr = idxm + 1
+  while amt > 0 and idxr != idxm:
+    if idxr >= len(inlst):
+      idxr = 0
+    
+    outlst[idxr] += 1
+    amt -=1
+    idxr +=1
+  
+  return outlst
+
+if __name__ == "__main__":
+  process(sys.argv[1])
