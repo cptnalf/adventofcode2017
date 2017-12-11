3e086e0f10cfe96238dd09128fdc384a6696d597
cptnalf <captainalf@gmail.com>
2017-12-11 12:51:34 -0500

    day 10 part 2 done.

--- 10/process.py
+++ 10/process.py
@@ -1,40 +1,88 @@
 #!/usr/bin/python
 import sys
 
-def process(lenlst):
-  arr = list(range(0,256))
-  pos = 0
-  step = 0
-  vals = []
-  idxs = []
+class KnotHash:
+  def __init__(self):
+    self.pos = 0
+    self.step = 0
+    self.round = 0
+    self.arr = list(range(0,256))
+    self.srclens = []
+  
+  def setsrc(self,src):
+    self.srclens = src
 
-  for x in lenlst:
-    vals.clear()
-    idxs.clear()
+  def gethex(self):
+    idx = 0
+    res = ''
 
-    cnt = x
-    idx = pos
-    while cnt > 0:
-      if idx >= len(arr):
-        idx = 0
-      
-      vals.append(arr[idx])
-      idxs.append(idx)
+    while idx < 16:
+      v = 0
+      for x in range(idx *16,(idx +1) * 16):
+        v ^= self.arr[x]
+      res += format(v, "02x")
       idx += 1
-      cnt -= 1
 
-    idxs.reverse()
-    for y in range(0,len(idxs)):
-      arr[idxs[y]] = vals[y]
+    return res
+
+  def pull(self, stringochars):
+    lst = list()
+    for x in stringochars:
+      lst.append(ord(x))
+
+    return lst
+
+  def doround(self):
+    self.round += 1
+
+    vals = []
+    idxs = []
+
+    for x in self.srclens:
+      vals.clear()
+      idxs.clear()
+
+      cnt = x
+      idx = self.pos
+      while cnt > 0:
+        idx = idx % 256
+        
+        vals.append(self.arr[idx])
+        idxs.append(idx)
+        idx += 1
+        cnt -= 1
+      
+      idxs.reverse()
+      for y in range(0, len(idxs)):
+        self.arr[idxs[y]] = vals[y]
+
+      # print("{0} {1} {2}  {3} => {4}".format(self.pos, self.step, x, idxs, vals))
+
+      self.pos += x + self.step
+      self.pos = self.pos % 256
+      self.step += 1
     
-    print("{0} {1} {2}  {3} => {4}".format(pos, step, x, idxs, vals))
 
-    pos += x + step
-    if pos >= len(arr):
-      pos -= len(arr)
-    step += 1
+def process(lenlst):
+  hasher = KnotHash()
+  hasher.setsrc(lenlst)
+  hasher.doround()
 
-  print(str(arr[0]) + " " + str(arr[1]) + " = " + str(arr[0] * arr[1]))
+  print(str(hasher.arr[0]) + " " + str(hasher.arr[1]) + " = " + str(hasher.arr[0] * hasher.arr[1]))
+
+def part2(instr):
+  hasher = KnotHash()
+  staticset = [17,31,73,47,23]
+
+  inchars = hasher.pull(instr)
+  inchars.extend(staticset)
+  hasher.setsrc(inchars)
+  while hasher.round < 64:
+    #print("round: {0} {1} {2}".format(hasher.round, hasher.pos, hasher.step ))
+    hasher.doround()
+    #print(hasher.gethex())
+  
+  print(hasher.gethex())
 
 def load(fname):
   res = []
@@ -51,5 +99,16 @@ def load(fname):
 if __name__ == "__main__":
   lst = load(sys.argv[1])
   process(lst)
+
+  str1 = ''
+  with open(sys.argv[1]) as f1:
+    str1 = f1.read().rstrip()
+  
+  part2('')
+  part2('AoC 2017')
+  part2('1,2,3')
+  part2('1,2,4')
+  part2(str1)
+
 
       
