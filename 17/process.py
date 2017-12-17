2c68c0f582cbf417edd71d1eee7a8722e097275e
cptnalf <captainalf@gmail.com>
2017-12-17 12:04:48 -0500

    day 17 part 1 testing works!

---
+++ 17/process.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+ # circular buffer.
+def step(lst, cnt, idx):
+  idx += cnt
+  if idx > len(lst):
+    idx = idx % len(lst)
+  
+  return idx
+
+buf = list()
+buf.append(0)
+
+newidx = 0
+stpcnt = 3
+curidx = 0
+for x in range(1,2018):
+  newidx = step(buf,stpcnt, curidx)
+  buf.insert(newidx,x)
+  curidx = newidx + 1
+
+print(buf)
